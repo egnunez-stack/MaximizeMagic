85df02e3d9789bd1981c44ac73b8bb03c14f1a48
egnunez <egnunez@gmail.com>
2026-01-08 09:23:32 -0300

    commit inicial

---
+++ composeApp/src/commonTest/kotlin/com/gen/maximizemagic/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.gen.maximizemagic
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/gen/maximizemagic/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.gen.maximizemagic
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
