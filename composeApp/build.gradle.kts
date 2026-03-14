de2024c4e1bf446b9d6c5017765c5f8d13d166e8
GEN <egnuenz@gmail.com>
2026-03-14 17:09:38 -0300

    Upgrade versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.serialization") version "2.1.0"
     alias(libs.plugins.google.services)
     kotlin("native.cocoapods")
 }
